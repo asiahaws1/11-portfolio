709e2d43240468ad36a4782b900c6fdce73a9a6a
asiahaws1 <144909095+asiahaws1@users.noreply.github.com>
2024-04-30 03:53:13 -0600

    init commit

---
+++ data/hotels.js
@@ -0,0 +1,66 @@
+export const hotels = [
+  
+    { 
+        "name": "Salt Lake City",
+        "photo": "slc-small.webp",
+        "address": ["500 Main St,", "Salt Lake City, UT 84101"],
+        "phone": "(801) 596-5700"
+        
+    },
+
+    { 
+        "name": "Japan",
+        "photo": "japan-small.webp",
+        "address": ["2 Chome-31-6 ,", "Taito City, Tokyo"],
+        "phone": "816-6463-5000"
+        
+    },
+
+    { 
+        "name": "London",
+        "photo": "london-small.webp",
+        "address": ["The Royal Victoria Dock,","London E16 1FA"],
+        "phone": "20 3637 7401"
+        
+    },
+
+    { 
+        "name": "Boston",
+        "photo": "boston-small.webp",
+        "address": ["250 O'Brien Highway,", "Cambridge, MA 02141"],
+        "phone": "(617) 822-3600"
+        
+    },
+
+    { 
+        "name": "Los Angeles",
+        "photo": "la-small.webp",
+        "address": ["416 W 8th St,", "Los Angeles, CA 90014"],
+        "phone": "(213) 612-0021"
+        
+    },
+
+    { 
+        "name": "Paris",
+        "photo": "paris-small.webp",
+        "address": ["Rue Saint-Séverin,", "75005 Paris, France"],
+        "phone": "33 14 6 34 570"
+        
+    },
+
+    { 
+        "name": "Berlin",
+        "photo": "berlin-small.webp",
+        "address": ["Potsdamer Platz 3,", "10785 Berlin, Germany"],
+        "phone": "49 30 337777"
+        
+    },
+
+    { 
+        "name": "Washington D.C.",
+        "photo": "washington-small.webp",
+        "address": ["1500 New Hampshire", "Washington, DC 20036"],
+        "phone": "(202) 483-6000"
+        
+    },
+]
